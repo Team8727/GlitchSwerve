0870f1d71eac0d30562beb89f6155e1b154f1dbc
VStellar2023 <naanuboy123@gmail.com>
2024-02-26 17:05:11 -0500

    Climber code with filter and current limit, with spotless

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,7 +34,7 @@ public class Constants {
     DESKTOP_VISION,
     DESKTOP
   }
-  
+
   // SysId
   public static enum sysIdType {
     LINEAR,
@@ -49,7 +49,7 @@ public class Constants {
     SHOOTERPIVOT,
     CLIMBER
   }
-  
+
   public static class kClimber {
     public static int climberID = 30;
 
@@ -61,7 +61,7 @@ public class Constants {
 
     public static final double kP = 0;
     public static final double kD = 0;
-    
+
     public static final double currentLimit = 0;
   }
 
